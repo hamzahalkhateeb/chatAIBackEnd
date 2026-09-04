993a1a9586005de0b4c02c28e671e5fac7d351b1
Hamzah <hamzahalkhateeb42@gmail.com>
2026-09-04 09:50:49 +0930

    added message and message reactions

---
+++ Models/Message.cs
@@ -0,0 +1,25 @@
+
+namespace backEnd.Models;
+
+public class Message
+{
+    public Guid Id {get; set;}
+
+    public Guid ChatId {get; set;}
+
+    public Guid SenderId {get; set;}
+
+    public string Body {get; set;} = string.Empty;
+
+    public Guid ReplyToMessageId {get; set;}
+
+    public DateTimeOffset  CreatedAt {get; set;}
+
+    public DateTimeOffset UpdatedAt {get; set;}
+    public DateTimeOffset DeletedAt {get; set;}
+
+    public bool IsEdited {get; set;} = false;
+    public bool IsDeleted {get; set;} = false;
+
+    public List<string> EditHistory {get; set;} = new();
+}
