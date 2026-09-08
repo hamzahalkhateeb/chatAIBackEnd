4b62f8d9a1b1f313b375635c8445115d15f0d0eb
Hamzah <hamzah.alkhateeb@synechron.com.au>
2026-09-08 15:21:23 +0930

    prepped log in endpoint, created email and password validation methods, next i will code find or create a user with email

---
+++ Modules/Authentication/AuthService.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using backEnd.Modules;
+using backEnd.Modules.Authentication;
+using backEnd.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace backEnd.Modules.Authentication
+{
+    public class AuthService
+    {
+        //look for an existing user with this email
+        
+
+        //create a new user using these details
+    }
+}
