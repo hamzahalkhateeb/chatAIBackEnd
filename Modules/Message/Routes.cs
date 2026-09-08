beceb3bfe797a11b9196a0b6fa45a5b68c7b629e
Hamzah <hamzahalkhateeb42@gmail.com>
2026-09-08 10:57:43 +0930

    further planning comments created

--- Modules/Message/Routes.cs
+++ Modules/Message/Routes.cs
@@ -1,1 +1,19 @@
 //almost all message functionality will be implemented in signal R, not REST API
+/*
+1- get message history - last 30 messages
+
+2- send a new message
+
+3- get single message
+
+4- edit own message
+
+5- delete or unsent message
+
+6- pin message
+
+*/
+
+
+///please note, scanninga message for keywords, putting a keyword in a category, creating a message flag are all part of this, will decide exactly where it goes as development goes
+/// 
--- Modules/Reactions/Routes.cs
+++ Modules/Reactions/Routes.cs
@@ -1,1 +1,11 @@
 //will be swapped for signal R
+
+/*
+1- list all reactions to a message
+
+2- add a reaction to a message
+
+3- remove your own message
+
+
+*/
