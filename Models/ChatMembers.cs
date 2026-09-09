c133e3acc840176e7a20af64c0fbb11c87916719
Hamzah <hamzah.alkhateeb@synechron.com.au>
2026-09-09 16:12:24 +0930

    indexed regularly frequented columns such id columns, email and username columns

--- Models/ChatMembers.cs
+++ Models/ChatMembers.cs
@@ -4,6 +4,7 @@ public class ChatMembers
 {
     public Guid Id { get; set; }
     public Guid UserId { get; set; }
+    public Guid ChatId{get; set;}
 
     //admin, regular, second hand etc
     //sub admin can have less powers like deleting messages, maybe name this mod for the lols
