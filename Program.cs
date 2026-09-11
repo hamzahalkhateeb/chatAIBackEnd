1d97001f8a30405ce807e2b89a9f2ff369630f0b
Hamzah <hamzahalkhateeb42@gmail.com>
2026-09-11 11:39:33 +0930

    log in endpoint seemingly completed, token and returns are also completed, will manually test it next

--- Program.cs
+++ Program.cs
@@ -55,7 +55,10 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngular", policy =>
     {
-        policy.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod();
+        policy.WithOrigins("http://localhost:4200")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
     });
 });
 
